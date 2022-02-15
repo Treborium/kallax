964ad045710a960362a33a3a78f322a09ac517f6
Robert Fuchs <robert.fuchs@logmein.com>
2022-02-15 11:26:59 +0100

    add chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 
+import { ChakraProvider } from '@chakra-ui/react';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -32,7 +34,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel='apple-touch-icon' href='/apple-icon.png'></link>
         <meta name='theme-color' content='#317EFB' />
       </Head>
-      <Component {...pageProps} />
+
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </>
   );
 }
